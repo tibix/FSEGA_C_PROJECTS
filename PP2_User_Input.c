4067153550c20ccd9c510d70154651c8a8c998f9
Tiberiu Dascal <tiberiu.purcel@gmail.com>
2022-02-25 10:31:13 +0200

    Am adaugat PP2_User_Input pentru a demostra colectarea informatiilor de la user si stocarea in variabile. De asemenea am demostrat si afisarea lor in terminal

---
+++ PP2_User_Input.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+int main(){
+
+	char raspuns;
+	char nume[30];
+	char prenume[30];
+	float inaltime;
+	int virsta;
+	double salariu;
+
+	// colecteaza numele
+	printf("Completeaza numele tau: ");
+	scanf("%s", nume);
+
+	// colecteaza prenumele
+	printf("Introdu prenumele tau: ");
+	scanf("%s", prenume);
+
+	// colecteaza virsta
+	printf("Introdu virsta curenta (partea intreaga): ");
+	scanf("%d", &virsta);
+
+	// colecteaza inaltimea
+	printf("Introdu inaltimea (m.cm): ");
+	scanf("%f", &inaltime);
+
+	//colecteaza salariul
+	printf("Introdu salariul lunar (cu maxim doua zecimale): ");
+	scanf("%lf", &salariu);
+
+	// prezinta datale in terminal:
+	printf("\nMultumim ca ai completat acest formular!\n");
+	printf("Te rugam sa verifici corectitudinea datelor:\n");
+	printf("\tNume     : %s\n", nume);
+	printf("\tPrenume  : %s\n", prenume);
+	printf("\tVirsta   : %d\n", virsta);
+	printf("\tInaltime : %f\n", inaltime);
+	printf("\tSalariu  : %lf\n", salariu);
+	printf("\n");
+
+	printf("Sunt corecte infromatiile? D (DA) sau N (NU): ");
+	scanf(" %c", &raspuns);
+
+	switch(raspuns){
+
+		case 'D':
+			printf("Datele au fost salvate cu succes!\n");
+			break;
+
+		case 'N':
+			printf("Eroare de sistem! Te rugam sa incerci mai tarziu!\n");
+			break;
+
+		default:
+			printf("Raspuns invalid! Te rugam sa incerci mai tarziu!\n");
+	}
+
+	return 0;
+}
--- README.md
+++ README.md
@@ -6,8 +6,12 @@ Pentru o organizare mai eficienta, proiectele cu prefixul **UP** vor fii cele in
 
 
 ## UP1_hello_world ##
-Un mic program care printeaza in terminal sirul de caractere "Hello World"
+Un mic program care printeaza in terminal sirul de caractere "Hello World".
 
 
 ## PP1_String_variables ##
-Un mic care demostreaza definirea variabilelor de tip string si afisarea lor in terminal
+Un mic care demostreaza definirea variabilelor de tip string si afisarea lor in terminal.
+
+## PP2_User_Input ##
+Program care preia informatia de la utilizator si apoi o afiseaza in terminal.
+Am folosit mai multe tiuri de date pentru a demonstra cum se colecteaza si salveaza aceastea in variabile.
